a307dc34d24c54f3991c09fe04077c849f69427f
nithincyberitus <nithin.gollapalli@cyber-itus.com>
2018-07-05 10:54:20 +0530

    Root Directory

---
+++ DigiLock/digilock_pkg/app.py
@@ -0,0 +1,6 @@
+
+def Main_Logic():
+    
+
+if __name__ == '__main__':
+    Main_Logic()
---
+++ DigiLock/digilock_pkg/ui_components/windowLayout_class.py
@@ -0,0 +1,8 @@
+# UI Imports
+
+# PySide2 Imports
+from PySide2.QtWidgets import QWidget,QHBoxLayout
+class Layout_Main_Window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        
