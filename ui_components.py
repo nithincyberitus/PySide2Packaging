2e9cbc8743188af4a3af450db09a0d0f8b736e87
nithincyberitus <nithin.gollapalli@cyber-itus.com>
2018-07-12 16:38:18 +0530

    Switching Between Widgets Achieved

--- ui_components.py
+++ ui_components.py
@@ -1,11 +1,13 @@
-from PySide2.QtWidgets import QMainWindow,QWidget,QFrame,QDesktopWidget,QHBoxLayout,QLabel,QFormLayout,QLineEdit,QPushButton
-from PySide2.QtCore import Qt,SIGNAL
+import sys
+from PySide2.QtWidgets import QMainWindow,QApplication,QHBoxLayout,QStackedWidget,QFormLayout
+from PySide2.QtWidgets import QWidget,QFrame,QDesktopWidget,QLabel,QLineEdit,QPushButton
+from PySide2.QtCore import Qt
 from PySide2.QtGui import QImage,QPixmap
 
 class Main_Window(QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
-        self.setWindowTitle("Login Window")
+        super(Main_Window,self).__init__()
+        self.setWindowTitle("Welcome Window")
         #self.setGeometry(350,250,550,400)
         self.fg = self.frameGeometry()
         self.cp = QDesktopWidget().availableGeometry().center()
@@ -31,7 +33,6 @@ class Main_Window(QMainWindow):
     #MenuBar
     def Create_MenuBar(self):
         return
-
     #StatusBar
     def Create_StatusBar(self):
         return
@@ -43,49 +44,77 @@ class Main_Window(QMainWindow):
         return
     #CentralWidget
     def Create_CentralWidget(self):
-        self.Main_Layout = Layout_Main_Window()
-        self.setCentralWidget(self.Main_Layout)
+        self.Main_Widget = Main_Widget()
+        self.setCentralWidget(self.Main_Widget)
 
-class Layout_Main_Window(QWidget):
-    def __init__(self):
-        QWidget.__init__(self)
-        self.Main_Layout = QHBoxLayout()
-        self.Child_1 = Layout_Child_1()
-        self.Child_2 = Layout_Child_2()
-        self.Main_Layout.addWidget(self.Child_1)
+class Main_Widget(QWidget):
+    def __init__(self,parent=None):
+        super(Main_Widget,self).__init__(parent)
+        self.setUp_UI()
+
+    def setUp_UI(self):
+        self.layout = QHBoxLayout()
+        self.stack = QStackedWidget(parent=self)
+        self.welcome = Welcome_Widget()
+        self.stack = QStackedWidget(parent=self)
+        self.welcome.login.Button_Login.clicked.connect(
+            self.check_user_details)
+        self.user = User_Widget(parent=self)
+        self.user.Button_Quit.clicked.connect(self.user_quit)
+        self.stack.addWidget(self.welcome)
+        self.stack.addWidget(self.user)
+        self.layout.addWidget(self.stack)
+
+    def check_user_details(self):
+        self.stack.setCurrentWidget(self.user)
+
+    def user_quit(self):
+        self.stack.setCurrentWidget(self.welcome)
+
+class Welcome_Widget(QWidget):
+    def __init__(self,parent=None):
+        super(Welcome_Widget,self).__init__(parent)
+        self.setUp_UI()
+
+    def setUp_UI(self):
+        self.image = Image_Widget(self)
         self.line = QFrame()
         self.line.setFrameShape(QFrame.VLine)
         self.line.setFrameShadow(QFrame.Sunken)
-        self.Main_Layout.addWidget(self.line)
-        self.Main_Layout.addWidget(self.Child_2)
-        self.setLayout(self.Main_Layout)
+        self.login = Login_Widget(self)
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.image)
+        self.layout.addWidget(self.line)
+        self.layout.addWidget(self.login)
 
-class Layout_Child_1(QFrame):
-    def __init__(self):
-        QFrame.__init__(self)
+        self.setLayout(self.layout)
+
+class Image_Widget(QWidget):
+    def __init__(self,parent=None):
+        super(Image_Widget,self).__init__(parent)
         self.setUp_GUI()
 
     def setUp_GUI(self):
         self.label = QLabel()
         self.image = QImage('images\sec_cyber.jpg')
         # To scale image for example and keep its Aspect Ration
-        #self.image.scaled(0,0,aspectRatioMode=Qt.IgnoreAspectRatio,transformMode=Qt.FastTransformation)
+        #self.image.scaled(10,10,aspectRatioMode=Qt.IgnoreAspectRatio,transformMode=Qt.FastTransformation)
         self.label.setPixmap(QPixmap.fromImage(self.image))
         self.layout = QHBoxLayout()
         self.layout.addWidget(self.label)
+        self.setFixedSize(260,360)
         self.setLayout(self.layout)
 
-
-class Layout_Child_2(QWidget):
+class Login_Widget(QWidget):
     '''Our Login Display Window'''
-    def __init__(self):
+    def __init__(self,parent=None):
         '''Constructor Function'''
-        QWidget.__init__(self)
+        super(Login_Widget,self).__init__(parent)
         self.setUp_Login_UI()
 
     def setUp_Login_UI(self):
         self.Login_Form = QFormLayout()
-        self.setFixedSize(250,250)
+        self.setFixedSize(260,360)
         self.Label_User_Name = QLabel("User Name:")
         self.Label_User_Password = QLabel("Password")
         self.Txt_User_Name = QLineEdit()
@@ -97,23 +126,11 @@ class Layout_Child_2(QWidget):
         self.Login_Form.addRow(self.Button_Login)
         self.Login_Form.setFormAlignment(Qt.AlignCenter)
         self.setLayout(self.Login_Form)
-        self.connect(self.Button_Login,SIGNAL("clicked()"),self.Check_Login_Details)
 
-    def Check_Login_Details(self):
-        
-        self.username = self.Txt_User_Name.text()
-        self.password = self.Txt_User_Password.text()
+class User_Widget(QWidget):
+    def __init__(self, parent):
+        super(User_Widget, self).__init__(parent)
+        self.init_UI()
 
-        url = 'http://127.0.0.1:5000/auth/user'
-        
-        payload = {'username':self.username,'password':self.password}
-        
-        resp = requests.post(url,params=payload)
-        #print(resp.status_code)
-
-        if resp.status_code == 200:
-            # Go to New Window
-            pass
-        else:
-            #Show Error
-            pass
+    def init_UI(self):
+        self.Button_Quit = QPushButton('Quit', parent=self)
